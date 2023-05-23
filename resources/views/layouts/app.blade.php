af0bf569c1b0e947ff2b96fe34a219c0449188a2
LookasWasTaken~ <piacen.luca@gmail.com>
2023-05-23 16:19:17 +0200

    ADD partials sponsor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,12 +6,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="../../img/favicon.ico" type="image/x-icon">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;400&family=Odibee+Sans&family=Roboto:wght@100;300;500&display=swap" rel="stylesheet">
     <title>DC Comics - @yield("pagetitle", "Default")</title>
     @vite('resources/js/app.js')
 </head>
 
 <body>
     @include("partials.header")
+    @include("partials.jumbo")
     <main>
         @yield("content")
     </main>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,3 @@
 <header id="header">
-    
+@include("partials.sponsor")
 </header>
